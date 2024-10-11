fb76228dc81ed3de4f0b0e7284383ef5d6b59fdf
Jms_Rea <125179253+Zesty-ZDev@users.noreply.github.com>
2024-10-12 09:22:42 +1000

    Update modalCreate.js
    Added account created date to staff verification embed

--- src/events/modalCreate.js
+++ src/events/modalCreate.js
@@ -30,7 +30,7 @@ module.exports = {
 
 
       const VerifyRequest = new EmbedBuilder()
-      .setDescription(`<@${interaction.user.id}> has requested verification and has set their name to: \n\`${NickNameString}\` \n\n *Click the "Verify" Button below to verify the user or click "Deny" to notify the user something is wrong* `) 
+      .setDescription(`<@${interaction.user.id}> has requested verification and has set their name to: \n\`${NickNameString}\` \n\nAccount created - <t:${Math.floor(interaction.user.createdTimestamp / 1000)}> \n\n *Click the "Verify" Button below to verify the user or click "Deny" to notify the user something is wrong* `) 
       .setTitle(`${client.user.username}`)
       .setColor(config.color)
       .setFooter({text:`${client.user.username} by Z-Dev`})
